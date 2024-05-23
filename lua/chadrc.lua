9e6598f10e6e391de6487e4e1eaf43c72ef122d0
inilov <tomederson92@gmail.com>
2024-05-23 18:53:54 +0300

    ArchChad v2 - personal modifications

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,16 +1,62 @@
--- This file  needs to have same structure as nvconfig.lua 
+-- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 M.ui = {
-	theme = "catppuccin",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+    theme = "gruvchad",
+    theme_toggle = {},
+    -- statusline
+    statusline = {
+        theme = "minimal", -- default/vscode/vscode_colored/minimal
+        -- default/round/block/arrow separators work only for default statusline theme
+        -- round and block will work for minimal theme only
+        separator_style = "default",
+        order = nil,
+        modules = nil,
+    },
+    cmp = {
+        style = "atom_colored",
+    },
+    nvdash = {
+        load_on_startup = true,
+        header = {
+            "           ▄ ▄                   ",
+            "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
+            "       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
+            "    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
+            "  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ",
+            "  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄",
+            "▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
+            "█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
+            "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
+        },
+        buttons = {
+            { "  Find File", "Spc ;", "Telescope find_files" },
+            { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+            { "󰈭  Find Word", "Spc f w n", "Telescope live_grep" },
+            { "  Bookmarks", "Spc f m", "Telescope marks" },
+            { "  Themes", "Spc t h", "Telescope themes" },
+            { "  Mappings", "Spc c h", "NvCheatsheet" },
+        },
+    },
+    term = {
+        hl = "Normal:term,WinSeparator:WinSeparator",
+        sizes = { sp = 0.2, vsp = 0.35 },
+        float = {
+            relative = "editor",
+            row = 0.3,
+            col = 0.25,
+            width = 0.5,
+            height = 0.4,
+            border = "single",
+        },
+    },
 }
+-- hl_override = {
+-- 	Comment = { italic = true },
+-- 	["@comment"] = { italic = true },
+-- },
 
 return M
--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,18 @@
 require "nvchad.options"
 
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.guicursor = ""
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 -- add yours here!
 
 -- local o = vim.o
